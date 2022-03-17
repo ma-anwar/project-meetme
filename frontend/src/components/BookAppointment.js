6c38d2a747fdb922a48d46a7a2b0190c00446448
mfwazir1 <mfwazir1@hotmail.com>
2022-03-16 20:29:04 -0400

    worked on booking

--- frontend/src/components/BookAppointment.js
+++ frontend/src/components/BookAppointment.js
@@ -1,13 +1,14 @@
 import './BookAppointment.css';
-import { useState, useEffect } from "react";
-import { Container, Typography, Grid, TextField, Button} from '@mui/material';
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import "react-big-calendar/lib/css/react-big-calendar.css"
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
-import enCA from 'date-fns/locale/en-CA'
+import { useState, useEffect } from 'react';
+import sx from 'mui-sx';
+import { Container, Typography, Box, TextField, Button } from '@mui/material';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from 'date-fns/getDay';
+import enCA from 'date-fns/locale/en-CA';
 
 const locales = {
   'en-CA': enCA,
@@ -21,76 +22,124 @@ const localizer = dateFnsLocalizer({
   locales,
 });
 
-const events = [
-    {
-        title: "Big Meeting",
-        allDay: true,
-        start: new Date(2022, 2, 0),
-        end: new Date(2022, 2, 1),
-    },
-    {
-        title: "Vacation",
-        start: new Date(2022, 2, 7),
-        end: new Date(2022, 2, 10),
-    },
-    {
-        title: "Conference",
-        start: new Date(2022, 2, 20),
-        end: new Date(2022, 2, 23),
-    },
+//Temporary, remove once retrieving appt info from BE
+const appointments = [
+  {
+    title: 'Big Meeting',
+    allDay: true,
+    start: new Date(2022, 2, 0),
+    end: new Date(2022, 2, 1),
+  },
+  {
+    title: 'Vacation',
+    start: new Date(2022, 2, 7),
+    end: new Date(2022, 2, 10),
+  },
+  {
+    title: 'Conference',
+    start: new Date(2022, 2, 20),
+    end: new Date(2022, 2, 23),
+  },
 ];
 
+const base = {
+  margin: 1,
+};
+
 export function BookAppointment() {
+  const [allAvailableAppts, setAvailableAppts] = useState(appointments);
+  // TODO ^ change above to retrieve all available appts for this event
+  const [book, setBook] = useState(false);
+  const [cmnt, setCmnt] = useState('');
+  const [who, setWho] = useState('Alice');
+  //TODO ^ change above to actual logged in user
+  const [when, setWhen] = useState(null);
 
-    //const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
-    const [allEvents, setAllEvents] = useState(events);
+  const eventTitle = 'This Event';
+  //TODO ^ change above to retrieve event title from BE
+  const eventDescription = 'Event Description';
+  //TODO ^ change above to retrieve event description from BE
 
-    // const [bookingDate, setBookingDate] = useState(null);
-    // const [selectedTimeSlot, setSelectedTimeSlot] = useState(null);
-    // const [bookingTimes, setBookingTimes] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    console.log('Booked');
+    //TODO send info to backend to actually book slot
+    //TODO, remove this slot from available slots
+  };
 
-    // useEffect(() => {
-    //     setBookingTimes(times);
-    // }, [bookingDate])
-    
-    // const onDateChange = e => {
-    //     setSelectedTimeSlot(null);
-    //     setBookingDate(e.value);
-    //   };
+  //TODO, change/chop format of start/end date
+  //Cur: Thu Mar 10 2022 00:00:00 GMT-0500 (Eastern Standard Time)
+  const bookAppt = ({ start, end }) => {
+    setWhen(start + ' - ' + end);
+    setBook(true);
+    //console.log(format(start, "yyyy-MM-dd'T'HH:mm:ss.SSS"));
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //e.target.reset();
-    };
+  const handleCancel = (e) => {
+    setCmnt('');
+    setBook(false);
+  };
 
-    const handleSelect = ({ start, end }) => {
-        const title = window.prompt('New Event name')
-        console.log(start);
-        console.log(end);
-        if (title){
-          const newEvent = {title, start, end}
-          setAllEvents([...allEvents, newEvent]);
-        }
-      }
+  const onCmntChange = (e) => {
+    setCmnt(e.target.value);
+  };
 
-       
-    return (
-    <Container className="BookAppt__container">
-        <Grid>
-            <Typography className="BookAppt__title">This Event</Typography>
-            <Typography className="BookAppt__description">Event Description</Typography>
-        </Grid>
-        <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500 }} 
-        selectable  onSelectEvent={event => alert(event.title)} onSelectSlot={handleSelect} />
-        <Grid>
-        <form className="BookAppt" onSubmit={handleSubmit}>
-            <TextField className="BookAppt__who" placeholder="Who" name="appt_booker" required/>
-            <TextField className="BookAppt__when" placeholder="When" name="appt_time" required/>
-            <TextField className="BookAppt__comments" placeholder="Comments" name="appt_cmnts" multiline rows={5}/>
-            <Button className="BookAppt_btn" type="submit" variant="contained">Book</Button>
-            <Button className="BookAppt_btn" type="button" variant="outlined">Cancel</Button>
+  return (
+    <Container>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography>{eventTitle}</Typography>
+        <Typography>{eventDescription}</Typography>
+      </Box>
+      <Calendar
+        localizer={localizer}
+        events={allAvailableAppts}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        selectable
+        onSelectEvent={bookAppt}
+      />
+      {book ? (
+        <form onSubmit={handleSubmit}>
+          <Box display="flex" flexDirection="column">
+            <TextField
+              sx={sx(base)}
+              placeholder="Who"
+              name="appt_booker"
+              value={who}
+              disabled
+            />
+            <TextField
+              sx={sx(base)}
+              placeholder="When"
+              name="appt_time"
+              value={when}
+              disabled
+            />
+            <TextField
+              sx={sx(base)}
+              placeholder="Comments"
+              name="appt_cmnts"
+              value={cmnt}
+              onChange={onCmntChange}
+              multiline
+              rows={5}
+            />
+            <Button sx={sx(base)} type="submit" variant="contained">
+              Book
+            </Button>
+            <Button
+              csx={sx(base)}
+              type="button"
+              variant="outlined"
+              onClick={handleCancel}
+            >
+              Cancel
+            </Button>
+          </Box>
         </form>
-        </Grid>  
+      ) : null}
     </Container>
   );
 }
--- frontend/src/components/EventCalendar.js
+++ frontend/src/components/EventCalendar.js
@@ -1,14 +1,16 @@
 import './EventCalendar.css';
-import { useState } from "react";
-import { Container, Typography, Grid, Button} from '@mui/material';
+import { useState } from 'react';
+import sx from 'mui-sx';
+import { Container, Typography, Box, Button } from '@mui/material';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import "react-big-calendar/lib/css/react-big-calendar.css"
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
-import enCA from 'date-fns/locale/en-CA'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import format from 'date-fns/format';
+import add from 'date-fns/add';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from 'date-fns/getDay';
+import enCA from 'date-fns/locale/en-CA';
 
 const locales = {
   'en-CA': enCA,
@@ -22,42 +24,90 @@ const localizer = dateFnsLocalizer({
   locales,
 });
 
-const events = [
-    {
-        title: "Big Meeting",
-        allDay: true,
-        start: new Date(2021, 6, 0),
-        end: new Date(2021, 6, 0),
-    },
-    {
-        title: "Vacation",
-        start: new Date(2021, 6, 7),
-        end: new Date(2021, 6, 10),
-    },
-    {
-        title: "Conference",
-        start: new Date(2021, 6, 20),
-        end: new Date(2021, 6, 23),
-    },
+const appointments = [
+  {
+    title: 'Big Meeting',
+    allDay: true,
+    start: new Date(2022, 2, 17),
+    end: new Date(2022, 2, 17),
+  },
+  {
+    title: 'Vacation',
+    start: new Date(2021, 6, 7),
+    end: new Date(2021, 6, 10),
+  },
+  {
+    title: 'Conference',
+    start: new Date(2021, 6, 20),
+    end: new Date(2021, 6, 23),
+  },
 ];
 
+const base = {
+  margin: 1,
+};
+
 export function EventCalendar() {
+  const [allAvailableAppts, setAvailableAppts] = useState(appointments);
+  // TODO ^ change above to retrieve all available appts for this event
 
-    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
-    const [allEvents, setAllEvents] = useState(events);
+  const eventTitle = 'This Event';
+  //TODO ^ change above to retrieve event title from BE
+  const eventUrl = 'Share link';
+  //TODO ^ change above to retrieve url from BE
 
-    function handleAddEvent() {
-        setAllEvents([...allEvents, newEvent]);
+  const handleSelect = ({ start, end }) => {
+    const slotLength = 30; //TODO, change to retrieve from BE
+    const title = eventTitle;
+    console.log(start);
+    console.log(end);
+
+    //const title = window.prompt('New Event name');
+    // if (title) {
+    //   const newEvent = { title, start, end };
+    //   setAvailableAppts([...allAvailableAppts, newEvent]);
+    // }
+    // ^ with something like this, the event shows up on screen, but below doesn't
+
+    let startTime = start;
+    let endTime = end;
+
+    while (startTime < endTime) {
+      const finTime = add(startTime, { minutes: slotLength });
+      const newSlot = { title, startTime, finTime };
+      setAvailableAppts([...allAvailableAppts, newSlot]);
+      startTime = finTime;
     }
-       
-    return (
-    <Container className="EventCal__container">
-        <Grid>
-            <Typography className="EventCal__title">This Event</Typography>
-            <Typography className="EventCal__link">Share link</Typography>
-        </Grid>
-        <Button variant="contained" href="/book_appt"><CalendarMonthIcon/>Book Appointment</Button>
-        <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500 }}/>
+    console.log(allAvailableAppts);
+  };
+
+  return (
+    <Container>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        sx={sx(base)}
+      >
+        <Typography>{eventTitle}</Typography>
+        <Typography>{eventUrl}</Typography>
+        {/* TODO, remove below once we have a way for Bob to view Alice's calendar and book appt */}
+        <Button variant="contained" href="/book_appt">
+          <CalendarMonthIcon />
+          Book Appointment
+        </Button>
+      </Box>
+
+      <Calendar
+        localizer={localizer}
+        events={allAvailableAppts}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        sx={{ margin: 2 }}
+        selectable
+        onSelectSlot={handleSelect}
+      />
     </Container>
   );
 }

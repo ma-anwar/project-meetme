f2a374d3571e827dec1bf07028662a9ed800e024
ma-anwar <ma.rizvi.anwar@gmail.com>
2022-03-27 04:17:21 -0400

    Configure prod for ws, upgrade deployment strat (#42)

--- backend/dockerbuild.sh
+++ backend/dockerbuild.sh
@@ -2,6 +2,7 @@
 sudo docker build -t manwar01/meetme-backend:latest . -f Dockerfile.prod
 if [ $? -eq 0 ]; then
     sudo docker push manwar01/meetme-backend:latest
+    curl -H "Authorization: Bearer SecretSecretIGottaKeepIt" https://manwar.dev/v1/update
 else
     echo Build failed
 fi
--- frontend/dockerbuild.sh
+++ frontend/dockerbuild.sh
@@ -2,6 +2,7 @@
 sudo docker build -t manwar01/meetme-frontend:latest .
 if [ $? -eq 0 ]; then
     sudo docker push manwar01/meetme-frontend:latest
+    curl -H "Authorization: Bearer SecretSecretIGottaKeepIt" https://manwar.dev/v1/update
 else
     echo Build failed
 fi
